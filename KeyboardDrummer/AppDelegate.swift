224b36ef12ab2e927878e2425963400047877a9f
“tomnvt” <tom.novota@gmail.com>
2018-04-08 11:20:15 +0200

    first commit

---
+++ KeyboardDrummer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeyboardDrummer
+//
+//  Created by NVT on 08.04.18.
+//  Copyright © 2018 NVT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeyboardDrummer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeyboardDrummer
+//
+//  Created by NVT on 08.04.18.
+//  Copyright © 2018 NVT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
